50927dcbef4c13abe0695fddd924e42b9eda5554
Xamoel <maximilien.maire1808@gmail.com>
2025-02-25 15:05:46 +0100

    stone fall

--- Assets/Scripts/CameraTrigger.cs
+++ Assets/Scripts/CameraTrigger.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class CameraTrigger : MonoBehaviour
 {
-    public CameraFollow cameraFollow;
+    public CameraFollow cameraFollow;  
+    public StoneSpawner stoneSpawner;  
     public float shakeDuration = 0.5f;
     public float shakeMagnitude = 0.2f;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player")) // Assure-toi que ton joueur a le tag "Player"
+        if (other.CompareTag("Player")) 
         {
             cameraFollow.ShakeCamera(shakeDuration, shakeMagnitude);
+            stoneSpawner.SpawnStones();
         }
     }
 }
