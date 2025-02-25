1df17ad6a6932c015ab35b2c4e82d355a363e6fc
Romain Poulain <romain.poulain@edu.esiee-it.fr>
2025-02-25 14:08:01 +0100

    remove z player

--- Assets/Scripts/playercontroller.cs
+++ Assets/Scripts/playercontroller.cs
@@ -5,13 +5,13 @@ public class PlayerController : MonoBehaviour
     public AudioSource audioSource;
     public AudioClip GravityDown;
     public AudioClip GravityUp;
-    
-    
+
     public float speed = 0.10f;
     public float gravityIntensity = 2.0f;
     public float cooldownTime = 0.5f;
     public CameraTr cameraShake;
     public bool up = false;
+    public Animator animator;
 
     private Rigidbody2D rb;
     private float nextGravityChangeTime = 0f;
@@ -20,40 +20,58 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         rb.gravityScale = gravityIntensity;
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
 
+
         float move = Input.GetAxis("Horizontal") * speed;
         transform.position += new Vector3(move, 0, 0);
-
+        if(move == 0){
+            animator.SetBool("Idle", true);
+            animator.SetBool("Move", false);
+        } else {
+            animator.SetBool("Idle", false);
+            animator.SetBool("Move", true);
+        }
 
         if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextGravityChangeTime)
         {
             rb.gravityScale *= -1;
             nextGravityChangeTime = Time.time + cooldownTime;
+
             if (up)
             {
                 audioSource.clip = GravityUp;
                 audioSource.Play();
+                transform.eulerAngles = new Vector3(0, 0, 0);
                 up = false;
             }
             else
             {
+                transform.eulerAngles = new Vector3(0, 0, 180);
                 audioSource.clip = GravityDown;
                 audioSource.Play();
                 up = true;
             }
         }
-    }
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
 
-        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Ceiling"))
+        Vector3 scale = transform.localScale;
+        float originalX = Mathf.Abs(scale.x);
+
+        if (move > 0)
         {
-            //cameraShake.TriggerShake();
+            scale.x = up ? -originalX : originalX;
         }
+        else if (move < 0)
+        {
+
+            scale.x = up ? originalX : -originalX;
+        }
+
+        transform.localScale = scale;
     }
 }
