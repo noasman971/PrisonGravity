b5663d5aec14f18cf72b2081be977c49eda9cf1e
Romain Poulain <romain.poulain@edu.esiee-it.fr>
2025-02-26 15:40:33 +0100

    relier les scenes en anglais

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;  
+using UnityEngine.SceneManagement;
+
 public class DoorScript : MonoBehaviour
 {
     public int keysRequired = 3;
+    public string sceneToLoad = "Couloir"; // Set the default scene name here
     private bool isOpen = false;
     
     private void OnTriggerEnter2D(Collider2D other)
@@ -28,9 +30,8 @@ public class DoorScript : MonoBehaviour
             Debug.Log("Door is opening!");
             KeyManager.instance.RemoveKeys(keysRequired);
             Destroy(gameObject);
-            SceneManager.LoadScene("Couloir");
-            
-            
+            SceneManager.LoadScene(sceneToLoad); // Load the scene based on the value set in the Inspector
         }
     }
 }
+
--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -2,9 +2,23 @@ using UnityEngine;
 
 public class GameOverManager : MonoBehaviour
 {
+    public static GameOverManager Instance;
     public GameObject gameOverPanel; 
     private bool gameOver = false;
 
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);  
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
     void Update()
     {
         if (GameObject.FindGameObjectWithTag("Player") == null && !gameOver)
