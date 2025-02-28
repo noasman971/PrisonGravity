fd7b1008fc9bb076595751172fddb4d51afa135c
zinackes <zinacke@gmail.com>
2025-02-28 10:33:21 +0100

    convert time

--- Assets/DeathAndTime.cs
+++ Assets/DeathAndTime.cs
@@ -23,6 +23,25 @@ public class DeathAndTime : MonoBehaviour
         PlayerPrefs.SetInt("seconds", seconds);
         PlayerPrefs.Save(); 
         
-        displayText.text = "Death: " + nbrDeath.ToString() + " Time: " + seconds.ToString() + " seconds";
+        displayText.text = "Death: " + nbrDeath.ToString() + " Time: " + FormatTime(seconds);
     }
+    
+    string FormatTime(int totalSeconds)
+    {
+        int hours = totalSeconds / 3600;
+        int minutes = (totalSeconds % 3600) / 60;
+        int seconds = totalSeconds % 60;
+
+        string formattedTime = "";
+
+        if (hours > 0)
+            formattedTime += hours + "h ";
+        if (minutes > 0 || hours > 0) 
+            formattedTime += minutes + "m ";
+        if (seconds > 0 || formattedTime == "") 
+            formattedTime += seconds + "s";
+
+        return formattedTime.Trim(); 
+    }
+
 }
